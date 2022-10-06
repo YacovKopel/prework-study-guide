95ac97edfb002bcc1b42b15b5be82a25a604a804
Yacov Kopel <kokokopel94@gmail.com>
2022-10-06 15:16:32 -0700

    added script in html

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+// if (topic === 'HTML') {
+//   console.log("Let's study HTML!");
+// } else if (topic === 'CSS') {
+//   console.log("Let's study CSS!");
+// } else if (topic === 'Git') {
+//   console.log("Let's study Git!");
+// } else if (topic === 'JavaScript') {
+//   console.log("Let's study JavaScript!");
+// } else {
+//   console.log('Please try again!');
+// }
+
+for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+// for (var x = 0; x < topics.length; x++){
+//     if (topics[x] === 'HTML') {
+//         console.log("Let's study HTML!");
+//       } else if (topics[x] === 'CSS') {
+//         console.log("Let's study CSS!");
+//       } else if (topics[x] === 'Git') {
+//         console.log("Let's study Git!");
+//       } else if (topics[x] === 'JavaScript') {
+//         console.log("Let's study JavaScript!");
+//       } else {
+//         console.log('Please try again!');
+//       }
+
+// }
